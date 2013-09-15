aee4ecde4fd94058c18145bb28c07dc7858a68de
mbhadania@smu.edu <mbhadania@smu.edu>
2013-09-15 16:30:19 -0500

    added code to check instruction type or commented line or null line

--- 06/driver/src/os/Driver.java
+++ 06/driver/src/os/Driver.java
@@ -19,14 +19,20 @@ public class Driver {
 
     public static void main(String[] args) {
 
+
+
+
+
+
+
         BufferedReader br = null;
 
         try {
 
-            String sCurrentLine;
+            String cl; // current line... cl is easy to write so...
 
             br = new BufferedReader(new FileReader("C:\\Users\\miten\\Documents\\GitHub\\src\\06\\add\\Add.asm")); // BufferReader to read file
-   
+
             File file = new File("C:\\Users\\miten\\Documents\\GitHub\\src\\06\\add\\Add.hack"); // file to write
             if (!file.exists()) { //if file doesnt exist then it will create a new file
                 file.createNewFile();
@@ -37,9 +43,35 @@ public class Driver {
 
             //C:\Users\miten\Documents\GitHub\src\06\add
 
-            while ((sCurrentLine = br.readLine()) != null) { // this whille loop will fetch or read line by line
-                System.out.println(sCurrentLine);
-                bw.write(sCurrentLine); // writing to destination file
+            while ((cl = br.readLine()) != null) { // this whille loop will fetch or read line by line
+                System.out.println(cl);
+                // checking whether is it instrution A or C or commented line or null line.
+
+                if (cl.equals("")) {
+                    System.out.println("null");
+                } else {
+
+                    if (cl.startsWith("@")) {
+                        System.out.println("This is A instruction");
+                    } else {
+                        if (cl.startsWith("//")) {
+                            System.out.println("This is commented line.. ");
+                        } else {
+                            System.out.println("This is C instruction");
+                        }
+                    }
+                }
+
+
+
+
+
+
+
+
+
+
+                bw.write(cl); // writing to destination file
                 bw.write("\n"); // new line
 
             }
@@ -48,7 +80,7 @@ public class Driver {
         } catch (IOException e) { // handles exception
             e.printStackTrace();
         } finally { // final block
-            try {
+            try { // another try catch block to catch exception of close method of buffer reader.
                 if (br != null) {
                     br.close(); // read buffer closing
                 }
